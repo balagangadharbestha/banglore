4e2d4a1a03f6fc07498fafcdd71b5f05976d7c05
balagangadharbestha <ec2-user@ip-172-31-3-251.ap-south-1.compute.internal>
2023-03-10 16:38:47 +0000

    merge

--- wallmart.java
+++ wallmart.java
@@ -2,5 +2,9 @@ i m working on java files
 i m updating java code
 i m updating in development branch
 i m updating in remote repo
+<<<<<<< HEAD
 check push from differnt branch again
 rebase
+=======
+merging conflicts
+>>>>>>> 26880b7 (merge conflicts)

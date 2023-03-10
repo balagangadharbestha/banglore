9194879d19ea5c76ac85e0a3395d7e27caa26baa
balagangadharbestha <ec2-user@ip-172-31-3-251.ap-south-1.compute.internal>
2023-03-10 16:38:47 +0000

    b

--- wallmart.java
+++ wallmart.java
@@ -3,8 +3,12 @@ i m updating java code
 i m updating in development branch
 i m updating in remote repo
 <<<<<<< HEAD
+<<<<<<< HEAD
 check push from differnt branch again
 rebase
+=======
+merging conflicts
+>>>>>>> 26880b7 (merge conflicts)
 =======
 merging conflicts
 >>>>>>> 26880b7 (merge conflicts)

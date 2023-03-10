3fe2824bf298bcdc83988d35601972372baceb39
balagangadharbestha <ec2-user@ip-172-31-3-251.ap-south-1.compute.internal>
2023-03-10 16:48:49 +0000

    conflicts resolved

--- wallmart.java
+++ wallmart.java
@@ -2,8 +2,5 @@ i m working on java files
 i m updating java code
 i m updating in development branch
 i m updating in remote repo
-<<<<<<< HEAD
 merging conflicts
-=======
 check push from differnt branch
->>>>>>> development

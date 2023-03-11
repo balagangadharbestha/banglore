a5a24608396c559f2865d28df84de86a3479a9e5
balagangadharbestha <ec2-user@ip-172-31-3-251.ap-south-1.compute.internal>
2023-03-11 06:43:37 +0000

    Revert "b"
    This reverts commit 9194879d19ea5c76ac85e0a3395d7e27caa26baa.

--- wallmart.java
+++ wallmart.java
@@ -3,12 +3,8 @@ i m updating java code
 i m updating in development branch
 i m updating in remote repo
 <<<<<<< HEAD
-<<<<<<< HEAD
 check push from differnt branch again
 rebase
-=======
-merging conflicts
->>>>>>> 26880b7 (merge conflicts)
 =======
 merging conflicts
 >>>>>>> 26880b7 (merge conflicts)
